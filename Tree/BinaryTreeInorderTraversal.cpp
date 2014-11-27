6463ce0b1f408e1e9e05011ea463ad235adac4b2
zhangkeplus <zhangkeplus@gmail.com>
2014-11-27 21:39:01 +0800

    inorder_traversal recursive&unrecursive

---
+++ Tree/BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> r;
+    vector<int> inorderTraversal(TreeNode *root) {
+        if(root){
+            inorderTraversal(root->left);
+            r.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return r;
+    }
+};
+
+
+
+
+class Solution {
+public:
+    vector<int> r;
+    stack<TreeNode *> S;
+    vector<int> inorderTraversal(TreeNode *root) {
+        if(!root){
+            return r;
+        }
+        while(!S.empty()){
+            S.pop();
+        }
+        
+        S.push(root);
+        while(!S.empty()){
+            TreeNode *temp = S.top();
+            //TreeNode *r = temp->right;
+            //TreeNode *l = temp->left;
+            if(temp->right){
+                S.pop();
+                S.push(temp->right);
+                S.push(temp);  //将改变temp和temp->right在栈中顺序
+                temp->right = NULL; //避免回溯时再次将temp->right读进栈
+            }
+            if(temp->left){
+                S.push(temp->left);
+                temp->left = NULL; //避免回溯时再次将temp->left读进栈 
+            }
+            else{
+                r.push_back(temp->val);
+                S.pop();
+            }
+        }
+        return r;
+    }
+};
